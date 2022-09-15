2cefdd1b665301981a57c8261648c57252384cea
charles-zablit <zablitcharles@gmail.com>
2022-09-15 15:10:22 +0200

    Fix: path

--- kaldi_starter.py
+++ kaldi_starter.py
@@ -20,13 +20,13 @@ logger = logging.getLogger(__name__)
 def start_kaldi(server, input, output, controlChannel, speaker, language):
     if language == 'German':
         model = 'kaldi_tuda_de_nnet3_chain2.yaml'
-        kaldiDir = 'source kms_env/bin/activate && . path.sh && python nnet3_model.py -m 0 -e -t -y models/%s --redis-server=%s --redis-audio=%s --redis-channel=%s --redis-control=%s -s="%s" -fpc 190' % (model, server, input, output, controlChannel, speaker)
+        kaldiDir = 'cd ./kaldi-model-server && source kms_env/bin/activate && . path.sh && python nnet3_model.py -m 0 -e -t -y models/%s --redis-server=%s --redis-audio=%s --redis-channel=%s --redis-control=%s -s="%s" -fpc 190' % (model, server, input, output, controlChannel, speaker)
     else:
         onlineConf = 'en_160k_nnet3chain_tdnn1f_2048_sp_bi/conf/online.conf'
         model = 'en_160k_nnet3chain_tdnn1f_2048_sp_bi.yaml'
-        kaldiDir = 'source kms_env/bin/activate && . path.sh && python nnet3_model.py -m 0 -e -t -o models/%s -y models/%s --redis-server=%s --redis-audio=%s --redis-channel=%s --redis-control=%s -s="%s" -fpc 190' % (onlineConf, model, server, input, output, controlChannel, speaker)
+        kaldiDir = 'cd ./kaldi-model-server && source kms_env/bin/activate && . path.sh && python nnet3_model.py -m 0 -e -t -o models/%s -y models/%s --redis-server=%s --redis-audio=%s --redis-channel=%s --redis-control=%s -s="%s" -fpc 190' % (onlineConf, model, server, input, output, controlChannel, speaker)
     chDir = './kaldi-model-server'
-    os.chdir(chDir)
+    # os.chdir(chDir)
     os.system(kaldiDir)
 
 
